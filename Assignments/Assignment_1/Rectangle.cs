d5aa58eff30ddd1738521d0b8d8f6e737aee2282
Edgar Santana <santanaprogrammer@gmail.com>
2022-01-23 15:59:10 -0500

    finished Rectangle

--- Assignments/Assignment_1/Rectangle.cs
+++ Assignments/Assignment_1/Rectangle.cs
@@ -6,6 +6,23 @@ namespace Assignment_1
 {
     class Rectangle
     {
+        private double height;
+        private double width;
 
+        public Rectangle(double height, double width)
+        {
+            this.height = height;
+            this.width = width;
+        }
+
+        public double perimeter()
+        {
+            return (2 * height) + (2 * width);
+        }
+
+        public double area()
+        {
+            return height * width;
+        }
     }
 }
