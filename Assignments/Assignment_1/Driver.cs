898fe8656bcb72db5c1ca2a8455fd78a1d57e6c7
Edgar Santana <santanaprogrammer@gmail.com>
2022-01-23 16:18:42 -0500

    step 2 done

--- Assignments/Assignment_1/Driver.cs
+++ Assignments/Assignment_1/Driver.cs
@@ -13,7 +13,17 @@ namespace Assignment_1
                 Circle circle = new Circle(i);
                 Console.WriteLine("Area of a circle with radius " + i + " is " +
                     circle.area() + " circumference is " + circle.circumference());
+            }
 
+            for (int i = 1; i < 4; i++)
+            {
+                for (int j = 1; j < 4; j++)
+                {
+                    Rectangle rectangle = new Rectangle(i, j);
+                    Console.WriteLine("The Area of a rectanlge " + i + " " +
+                        j + " is " + rectangle.area() + " and its perimeter is " +
+                        rectangle.perimeter());
+                }
             }
         }
     }
