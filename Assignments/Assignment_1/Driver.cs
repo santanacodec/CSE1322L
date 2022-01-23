4c98fbccf684e053df071bb09ac4e444eb226e64
Edgar Santana <santanaprogrammer@gmail.com>
2022-01-23 16:11:25 -0500

    step 1 complete

--- Assignments/Assignment_1/Driver.cs
+++ Assignments/Assignment_1/Driver.cs
@@ -7,6 +7,14 @@ namespace Assignment_1
         static void Main(string[] args)
         {
             
+
+            for (int i = 1; i < 11; i++)
+            {
+                Circle circle = new Circle(i);
+                Console.WriteLine("Area of a circle with radius " + i + " is " +
+                    circle.area() + " circumference is " + circle.circumference());
+
+            }
         }
     }
 }
