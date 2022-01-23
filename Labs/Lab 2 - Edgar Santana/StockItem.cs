a398a8c0b3475643e88ed97a13ae9a1fa7eca27c
Edgar Santana <santanaprogrammer@gmail.com>
2022-01-23 17:28:02 -0500

    finished getters

--- Labs/Lab 2 - Edgar Santana/StockItem.cs
+++ Labs/Lab 2 - Edgar Santana/StockItem.cs
@@ -10,5 +10,40 @@ namespace Lab_2___Edgar_Santana
         static int id;
         double price;
         int quantity;
+
+        public StockItem() { }
+
+        public StockItem(string description, double price, int quantity)
+        {
+            this.description = description;
+            this.price = price;
+            this.quantity = quantity;
+        }
+
+        public string getDescription()
+        {
+            return description;
+        }
+
+        public int getId()
+        {
+            return id;
+        }
+
+        public double getPrice()
+        {
+            return price;
+        }
+
+        public int getQuantity()
+        {
+            return quantity;
+        }
+
+        public override string ToString()
+        {
+            return "id: " + id + "\ndescription: " + description +
+                "\nprice: " + price + "\nquantity: " + quantity;
+        }
     }
 }
