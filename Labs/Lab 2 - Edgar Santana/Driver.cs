f4634780b765e9bd8bf77767d2a46be0e3114cf1
Edgar Santana <santanaprogrammer@gmail.com>
2022-01-23 17:49:45 -0500

    finished first step in Driver

--- Labs/Lab 2 - Edgar Santana/Driver.cs
+++ Labs/Lab 2 - Edgar Santana/Driver.cs
@@ -2,11 +2,22 @@
 
 namespace Lab_2___Edgar_Santana
 {
-    class Program
+    class Driver
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            StockItem milk = new StockItem("1 Gallon of Milk", 3.60, 15);
+            StockItem bread = new StockItem("1 Loaf of Bread", 1.98, 30);
+
+            Console.WriteLine("Please select a number from our menu");
+            Console.WriteLine("1. Sold One Milk");
+            Console.WriteLine("2. Sold One Bread");
+            Console.WriteLine("3. Change price of Milk");
+            Console.WriteLine("4. Change price of Bread");
+            Console.WriteLine("5. Add Milk to Inventory");
+            Console.WriteLine("6. Add Bread to Inventory");
+            Console.WriteLine("7. See Inventory");
+            Console.WriteLine("8. Quit");
         }
     }
 }
