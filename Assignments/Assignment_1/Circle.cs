c4206add66945af1e1b2021dd12a154f9b1b7bb2
Edgar Santana <santanaprogrammer@gmail.com>
2022-01-23 15:49:31 -0500

    finished Circle

--- Assignments/Assignment_1/Circle.cs
+++ Assignments/Assignment_1/Circle.cs
@@ -7,5 +7,20 @@ namespace Assignment_1
     class Circle
     {
         private double radius;
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public double circumference()
+        {
+            return 2 * Math.PI * radius;
+        }
+
+        public double area()
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
     }
 }
