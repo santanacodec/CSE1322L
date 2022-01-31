d2f2d370908163037f34b385612180da34672cee
Edgar Santana <santanaprogrammer@gmail.com>
2022-01-30 21:07:53 -0500

    Update Question.cs

--- Labs/Lab 3 - Edgar Santana/Question.cs
+++ Labs/Lab 3 - Edgar Santana/Question.cs
@@ -6,5 +6,51 @@ namespace Lab_3___Edgar_Santana
 {
     class Question
     {
+        private string question;
+        private string answer;
+        private int difficulty;
+
+        public Question(string question, string answer, int difficulty)
+        {
+            this.question = question;
+            this.answer = answer;
+            this.difficulty = difficulty;
+        }
+
+        public string getQuestion()
+        {
+            return question;
+        }
+
+        public string getAnswer()
+        {
+            return answer;
+        }
+
+        public int getDifficulty()
+        {
+            return difficulty;
+        }
+
+        public void setQuestion(string question)
+        {
+            this.question = question;
+        }
+
+        public void setAnswer(string answer)
+        {
+            this.answer = answer;
+        }
+
+        public void setDifficulty(int difficulty)
+        {
+            this.difficulty = difficulty;
+        }
+
+        public override string ToString()
+        {
+            return "Question: " + question + "\nAnswer: " + answer +
+                "\nDifficulty: " + difficulty;
+        }
     }
 }
