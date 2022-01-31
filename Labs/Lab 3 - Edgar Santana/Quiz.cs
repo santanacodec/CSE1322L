79b98006afc287ae40e0a329e2a511a7226be17f
Edgar Santana <santanaprogrammer@gmail.com>
2022-01-30 21:31:14 -0500

    Update Quiz.cs
    finished addQuestion()

--- Labs/Lab 3 - Edgar Santana/Quiz.cs
+++ Labs/Lab 3 - Edgar Santana/Quiz.cs
@@ -6,6 +6,26 @@ namespace Lab_3___Edgar_Santana
 {
     class Quiz
     {
+        private List<Question> quizMaker = new List<Question>();
 
+        public void addQuestion()
+        {
+            string question;
+            string answer;
+            int difficulty;
+
+            Console.WriteLine("What is your question?");
+            question = Console.ReadLine();
+
+            Console.WriteLine("What is the answer?");
+            answer = Console.ReadLine();
+
+            Console.WriteLine("What is the level of difficulty for your question?");
+            difficulty = Convert.ToInt32(Console.ReadLine());
+
+            Question quest = new Question(question, answer, difficulty);
+            quizMaker.Add(quest);
+
+        }
     }
 }
