e79c71723e6ed046a0bc79c808f29e2e19dc1d88
Edgar Santana <santanaprogrammer@gmail.com>
2022-02-05 03:40:49 -0500

    finished checking

---
+++ Labs/Lab 4 - Edgar Santana/Checking.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_4___Edgar_Santana
+{
+    class Checking : Account
+    {
+        double overdraftLimit = 0;
+        public Checking(double checkingBalance) : base(checkingBalance) { }
+
+        public override void withdraw(double wAmount) {
+            if (getAccountBalance() - wAmount > overdraftLimit)
+            {
+                setAccountBalance(getAccountBalance() - wAmount);
+            } else if (getAccountBalance() - wAmount < overdraftLimit)
+            {
+                Console.WriteLine("Charging an overdraft fee of $20 because" +
+                    " account is below $0");
+                setAccountBalance(getAccountBalance() - 20);
+            }
+        }
+    }
+}
