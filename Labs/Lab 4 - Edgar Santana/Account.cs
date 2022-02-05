dea4aad05981f4ae14c442555351b8e867603f54
Edgar Santana <santanaprogrammer@gmail.com>
2022-02-05 03:06:32 -0500

    finished Account class

---
+++ Labs/Lab 4 - Edgar Santana/Account.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_4___Edgar_Santana
+{
+    class Account
+    {
+        Random rnd = new Random();
+        static int accountNumber;
+        double accountBalance;
+
+        public Account()
+        {
+            accountNumber = rnd.Next();
+            accountBalance = 0;
+        }
+
+        public Account(double accountBalance)
+        {
+            this.accountBalance = accountBalance;
+            accountNumber = rnd.Next();
+        }
+
+        public int getAccountNumber()
+        {
+            accountNumber = rnd.Next();
+            return accountNumber;
+        }
+
+        public double getAccountBalance()
+        {
+            return accountBalance;
+        }
+
+        public void setAccountBalance(double accountBalance)
+        {
+            this.accountBalance = accountBalance;
+        }
+
+        public void withdraw(double wAmount)
+        {
+            accountBalance -= wAmount;
+        }
+
+        public double deposit(double dAmount)
+        {
+            return accountBalance + dAmount;
+        }
+    }
+}
