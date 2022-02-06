933d13438505d648554ee94615b91ab8beca7514
Edgar Santana <santanaprogrammer@gmail.com>
2022-02-06 16:03:10 -0500

    Update Driver.cs
    finished menu

--- Labs/Lab 4 - Edgar Santana/Driver.cs
+++ Labs/Lab 4 - Edgar Santana/Driver.cs
@@ -8,6 +8,20 @@ namespace Lab_4___Edgar_Santana
         {
             Checking checking = new Checking(0);
             Savings savings = new Savings(500);
+
+            Console.WriteLine("What would you like to do?");
+
+            Console.WriteLine("1. Withdraw from Checking");
+            Console.WriteLine("2. Withdraw from Savings");
+            Console.WriteLine("3. Deposit to Checking");
+            Console.WriteLine("4. Deposit to Savings");
+            Console.WriteLine("5. Balance of Checking");
+            Console.WriteLine("6. Balance of Savings");
+            Console.WriteLine("7. Award Interest to Savings now");
+            Console.WriteLine("8. Quit");
+
+
+
         }
     }
 }
