103398b1e7eae67243b46f3260616ef06726edb0
Edgar Santana <santanaprogrammer@gmail.com>
2022-02-06 21:26:59 -0500

    Update Employee.cs
    finished employee class

--- Assignments/Assignment_3/Employee.cs
+++ Assignments/Assignment_3/Employee.cs
@@ -12,7 +12,44 @@ namespace Assignment_3
 
         public Employee()
         {
+            name = "New Employee";
+            employeeID = ++numberOfEmployees;
+        }
 
+        public Employee(string name)
+        {
+            this.name = name;
+            employeeID = ++numberOfEmployees;
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public int getEmployeeID()
+        {
+            return employeeID;
+        }
+
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+
+        public void setEmployeeID(int employeeID)
+        {
+            this.employeeID = employeeID;
+        }
+
+        public static int getNumberOfEmployees()
+        {
+            return numberOfEmployees;
+        }
+
+        public static void decreaseNumberOfEmployees()
+        {
+            numberOfEmployees--;
         }
     }
 }
