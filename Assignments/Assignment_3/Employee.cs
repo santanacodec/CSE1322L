6002732c54b4d55e59f8f874f2bd5a1fe8c36950
Edgar Santana <santanaprogrammer@gmail.com>
2022-02-06 21:12:12 -0500

    started employee class

---
+++ Assignments/Assignment_3/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_3
+{
+    class Employee
+    {
+        private string name;
+        private int employeeID;
+        private static int numberOfEmployees = 0;
+
+        public Employee()
+        {
+
+        }
+    }
+}
