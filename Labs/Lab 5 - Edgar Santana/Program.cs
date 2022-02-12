abbcfe428a0b0206a57dd4217f6f00efd3f1ed45
Edgar Santana <santanaprogrammer@gmail.com>
2022-02-12 13:45:53 -0500

    started lab 5

---
+++ Labs/Lab 5 - Edgar Santana/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab_5___Edgar_Santana
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
