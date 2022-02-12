7f8d4bd8dbcc273e4456aa67fcd9f61695b400aa
Edgar Santana <santanaprogrammer@gmail.com>
2022-02-12 18:17:24 -0500

    Update Item.cs
    finished class

--- Labs/Lab 5 - Edgar Santana/Item.cs
+++ Labs/Lab 5 - Edgar Santana/Item.cs
@@ -16,5 +16,22 @@ namespace Lab_5___Edgar_Santana
         {
             this.title = title;
         }
+
+        public string getTitle()
+        {
+            return title;
+        }
+
+        public void setTitle(string title)
+        {
+            this.title = title;
+        }
+
+        public abstract void getListing();
+
+        public override string ToString()
+        {
+            return title;
+        }
     }
 }
