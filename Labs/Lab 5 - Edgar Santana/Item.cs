907f5e323aecee28bf7430d87490c550210b8ba6
Edgar Santana <santanaprogrammer@gmail.com>
2022-02-12 14:09:55 -0500

    started abstract class Item

---
+++ Labs/Lab 5 - Edgar Santana/Item.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_5___Edgar_Santana
+{
+    abstract class Item
+    {
+        private string title;
+        public Item()
+        {
+            title = "";
+        }
+
+        public Item(string title)
+        {
+            this.title = title;
+        }
+    }
+}
