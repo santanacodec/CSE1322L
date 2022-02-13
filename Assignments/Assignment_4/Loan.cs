2abdb01f6f8e9ec816b4bc8df188377b17f45def
Edgar Santana <santanaprogrammer@gmail.com>
2022-02-13 17:52:21 -0500

    Create Loan.cs

---
+++ Assignments/Assignment_4/Loan.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_4
+{
+    class Loan
+    {
+        private string customerName;
+        private int accountNumber;
+        private double loanBalance;
+        private double monthlyPayment;
+        private double interestRate;
+        private int numberOfMonthlyPayments;
+        private static int numberOfLoans = 0;
+
+        public Loan(string name, double amount, double rate, int months)
+        {
+            this.customerName = name;
+            this.loanBalance = amount;
+            this.interestRate = rate;
+            this.numberOfMonthlyPayments = months;
+            accountNumber = ++numberOfLoans;
+        }
+
+        public static int getNumberOfLoans()
+        {
+            return numberOfLoans;
+        }
+    }
+}
