8e7f4d569795fdc244c4ce9550a24bae6616806c
Edgar Santana <santanaprogrammer@gmail.com>
2022-02-13 17:00:18 -0500

    Finished

--- Labs/Lab 5 - Edgar Santana/myCollection.cs
+++ Labs/Lab 5 - Edgar Santana/myCollection.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Lab_5___Edgar_Santana
 {
@@ -11,11 +12,37 @@ namespace Lab_5___Edgar_Santana
             {
                 Console.WriteLine("Please enter B for Book or P for Periodical.");
                 char BorP = Convert.ToChar(Console.ReadLine());
+
+                if (BorP == 'B')
+                {
+                    Console.WriteLine("Please enter the name of the Book");
+                    string bookName = Console.ReadLine();
+
+                    Console.WriteLine("Please enter the author of the Book");
+                    string authorName = Console.ReadLine();
+
+                    Console.WriteLine("Please enter the ISBN of the Book");
+                    long isbnNum = Convert.ToInt64(Console.ReadLine());
+
+                    itemArray[i] = new Book(bookName, isbnNum, authorName);
+                }
+                else if (BorP == 'P')
+                {
+                    Console.WriteLine("Please enter the name of the Periodical");
+                    string periodicalName = Console.ReadLine();
+
+                    Console.WriteLine("Please enter the issue Number");
+                    int issueNumber = Convert.ToInt32(Console.ReadLine());
+
+                    itemArray[i] = new Periodical(periodicalName, issueNumber);
+                }
             }
-            
 
-            
-
+            Console.WriteLine("Your Items: ");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(itemArray[i].getListing());
+            }
         }
     }
 }
