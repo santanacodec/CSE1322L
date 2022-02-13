ec94014ff59939581d7bfa17aef6db41794156d9
Edgar Santana <santanaprogrammer@gmail.com>
2022-02-13 16:12:30 -0500

    update

--- Labs/Lab 5 - Edgar Santana/myCollection.cs
+++ Labs/Lab 5 - Edgar Santana/myCollection.cs
@@ -7,6 +7,15 @@ namespace Lab_5___Edgar_Santana
         static void Main(string[] args)
         {
             Item[] itemArray = new Item[5];
+            for(int i = 0; i < 5; i++) 
+            {
+                Console.WriteLine("Please enter B for Book or P for Periodical.");
+                char BorP = Convert.ToChar(Console.ReadLine());
+            }
+            
+
+            
+
         }
     }
 }
