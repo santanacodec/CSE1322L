2c7b7ca5e12e2f9afb0151d2b2a2d44004410642
Edgar Santana <santanaprogrammer@gmail.com>
2022-02-13 19:00:16 -0500

    started StudentLoan

---
+++ Assignments/Assignment_4/StudentLoan.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_4
+{
+    class StudentLoan : Loan
+    {
+        private bool isDeferred;
+
+        public StudentLoan(string name, double amount, double rate, int months, bool isDef) : 
+            base(name, amount, rate, months)
+        {
+            this.isDeferred = isDef;
+        }
+
+        public bool getIsDeferred()
+        {
+            return isDeferred;
+        }
+
+        public void setIsDeferred(bool isDeferred)
+        {
+            this.isDeferred = isDeferred;
+        }
+    }
+}
