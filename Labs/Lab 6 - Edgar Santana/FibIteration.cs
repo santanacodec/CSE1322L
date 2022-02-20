169f9bd749f3baaef973db23343e0e5688b23a3e
Edgar Santana <santanaprogrammer@gmail.com>
2022-02-19 20:30:03 -0500

    update

---
+++ Labs/Lab 6 - Edgar Santana/FibIteration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_6___Edgar_Santana
+{
+    class FibIteration
+    {
+
+    }
+}
---
+++ Labs/Lab 6 - Edgar Santana/FindFib.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_6___Edgar_Santana
+{
+    interface FindFib
+    {
+        public int calculate_fib(int n)
+        {
+            return n;
+        }
+    }
+}
