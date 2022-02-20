1157cbabba77151d0ec461a90b6cdc7ae3c3ebd4
Edgar Santana <santanaprogrammer@gmail.com>
2022-02-20 14:55:38 -0500

    finished

---
+++ Labs/Lab 6 - Edgar Santana/Driver.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lab_6___Edgar_Santana
+{
+    class Driver
+    {
+        static void Main(string[] args)
+        {
+            FibIteration fibIt = new FibIteration();
+            FibFormula fibForm = new FibFormula();
+            Console.WriteLine("Enter the number you want to find the Fibonacci Series for: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Fib of " + num + " by iteration is: " + fibIt.calculate_fib(num));
+            Console.WriteLine("Fib of " + num + " by formula is: " + fibForm.calculate_fib(num));
+        }
+    }
+}
--- Labs/Lab 6 - Edgar Santana/FibFormula.cs
+++ Labs/Lab 6 - Edgar Santana/FibFormula.cs
@@ -6,9 +6,9 @@ namespace Lab_6___Edgar_Santana
 {
     class FibFormula : IFindFib
     {
-        public int calculate_Fib(int n)
+        public int calculate_fib(int n)
         {
-
+            return Convert.ToInt32((Math.Pow(((1 + Math.Sqrt(5)) / 2), n) - Math.Pow(((1 - Math.Sqrt(5)) / 2), n)) / Math.Sqrt(5));
         }
     }
 }
