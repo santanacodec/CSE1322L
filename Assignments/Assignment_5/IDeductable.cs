d71e8d7d678f0b44cf8635a9e52d013e052da5f7
Edgar Santana <santanaprogrammer@gmail.com>
2022-02-20 20:42:53 -0500

    finished IDeductable

---
+++ Assignments/Assignment_5/IDeductable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_5
+{
+    interface IDeductable
+    {
+        abstract bool hasMetDeductible();
+        abstract bool hasMetTotalOutOfPocket();
+    }
+}
