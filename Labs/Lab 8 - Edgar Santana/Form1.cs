498931efe9f9291caf763121a8c4312ff78fca4f
Edgar Santana <santanaprogrammer@gmail.com>
2022-02-27 04:38:53 -0500

    finished

--- Labs/Lab 8 - Edgar Santana/Form1.cs
+++ Labs/Lab 8 - Edgar Santana/Form1.cs
@@ -16,5 +16,20 @@ namespace Lab_8___Edgar_Santana
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int input1 = int.Parse(textBox1.Text);
+            int input2 = int.Parse(textBox2.Text);
+
+            int output = input1 + input2;
+
+            label1.Text = output.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = " ";
+        }
     }
 }
