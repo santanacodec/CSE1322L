da25633ee7a4cde2a0e524f8edaeb1857725e40b
Edgar Santana <santanaprogrammer@gmail.com>
2022-03-27 11:18:50 -0400

    started recursion lab

---
+++ Labs/Lab 9a - Edgar Santana/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Lab_9a___Edgar_Santana
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int recursive_multiply(int a, int b)
+        {
+            int answer;
+
+            if (b == 0)
+            {
+                answer = 0;
+            }
+            else if (b == 1)
+            {
+                answer = a;
+            }
+            else
+            {
+                answer = a + recursive_multiply(a, b - 1);
+            }
+            return answer;
+        }
+    }
+}
