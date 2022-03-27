a2d01138eb071201e824a05273dc7b8f72125089
Edgar Santana <santanaprogrammer@gmail.com>
2022-03-27 17:22:02 -0400

    started lab9b

---
+++ Labs/Lab 9b - Edgar santana/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lab_9b___Edgar_santana
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static void repeatNTimes(string s, int i)
+        {
+
+        }
+    }
+}
