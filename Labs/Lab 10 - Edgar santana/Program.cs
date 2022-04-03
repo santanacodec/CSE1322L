45d51566383be927336474739e3601cbc2f9ee52
Edgar Santana <santanaprogrammer@gmail.com>
2022-04-03 17:50:46 -0400

    started lab

---
+++ Labs/Lab 10 - Edgar santana/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab_10___Edgar_santana
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static int format(string s)
+        {
+            int hours;
+            int minutes;
+            int seconds;
+            List<string> list = new List<string>();
+            
+            list = s.Split(":", 3).ToList();
+
+            hours = Convert.ToInt32(list[0]);
+            minutes = Convert.ToInt32(list[1]);
+            seconds = Convert.ToInt32(list[2]);
+             
+
+        }
+    }
+}
